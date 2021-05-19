06216b06673fb8aef968d10ba2ac9b3603603b36
Andrew-Yasser <49915521+Andrew-Yasser@users.noreply.github.com>
2021-05-19 03:36:39 +0200

    Update and rename assignment 1 .py to SomeFunctions_implementations_OpenCV .py

--- SomeFunctions_implementations_OpenCV .py
+++ SomeFunctions_implementations_OpenCV .py
@@ -3,19 +3,27 @@ import numpy as np
 from matplotlib import pyplot as plt
 img= plt.imread('Input.jpg')
 plt.imshow(img)
-plt.title('original')
+plt.title('original') #plotting the original image
+
+#median filter
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 median = cv2.medianBlur(gray,5)
 plt.imshow(median, cmap='gray')
 plt.title('median')
+
+#Histogram Equalization
 equ= cv2.equalizeHist(median)
 plt.imshow(equ, cmap='gray')
 plt.title('Histogram Equalization')
+
+#Adaptive Thresholding “on grayscale”
 thresh = cv2.adaptiveThreshold( median 
 ,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
  cv2.THRESH_BINARY,11,2)
 plt.imshow(thresh, cmap='gray')
 plt.title('Adaptive Thresholding “on grayscale”')
+
+#Sharpening image
 sharp=cv2.GaussianBlur(median,(5,5),0)
 cv2.blur(median, (5,5))
 plt.imshow(sharp, cmap='gray')
